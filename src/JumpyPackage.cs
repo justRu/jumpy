404bfc624168c30d0eecd6672e39aadce315a408
Sergey Zhezhel <just.ru@gmail.com>
2020-04-16 23:42:26 -0700

    upgrade to target VS2019

---
+++ src/JumpyPackage.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Jumpy
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(JumpyPackage.PackageGuidString)]
+    public sealed class JumpyPackage : AsyncPackage
+    {
+        public const string PackageGuidString = "bd1c14e2-a4d1-4483-990f-97766a593a5b";
+
+        protected override async System.Threading.Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+    }
+}
